835c8fde01f7386382a3043d34f83a3bcf09c64b
Daniel Nilsson <danielnil@proton.me>
2026-09-14 11:12:53 +0200

    "Lab3 Challenge finished"

--- fundamentals/lab3_challenge.py
+++ fundamentals/lab3_challenge.py
@@ -13,17 +13,21 @@ flights = [
     {'flight' : 'AY868', 'destination' : 'Helsinki', 'departure' : '20:55', 'gate' : '17', 'passengers' : 372, 'max_cap' : 380, 'delay' : -9, 'cancelled' : False},
     {'flight' : 'FR4013', 'destination' : 'Alicante', 'departure' : '21:10', 'gate' : '20A', 'passengers' : 362, 'max_cap' : 380, 'delay' : 25, 'cancelled' : False},
     {'flight' : 'WF209', 'destination' : 'Bergen', 'departure' : '22:05', 'gate' : '14', 'passengers' : 378, 'max_cap' : 380, 'delay' : -1, 'cancelled' : False},
-    {'flight' : 'FR4011', 'destination' : 'Krakow', 'departure' : '23:15', 'gate' : '13', 'passengers' : 345, 'max_cap' : 380, 'delay' : 121, 'cancelled' : False}
+    {'flight' : 'FR4011', 'destination' : 'Krakow', 'departure' : '23:15', 'gate' : '', 'passengers' : 345, 'max_cap' : 380, 'delay' : 121, 'cancelled' : True}
 ]
 
 #Part 2
-def departure_board():
+def departure_board(only_can=False,only_delay=False):
     for i,flight in enumerate(flights):
+        if only_can and not flight['cancelled']:
+            continue
+        if only_delay and not flight['delay'] > 0:
+            continue
         print(  f'{'{:>2}'.format(i+1)}. '
                 f'{'{:<7}'.format(flight['flight'])}- '
                 f'{'{:<10}'.format(flight['destination'])}- '
                 f'{'{:<6}'.format(flight['departure'])}- '
-                f'Gate {'{:<4}'.format(flight['gate'])}- '
+                f'Gate {'{:<5}'.format(get_gate(flight['gate']))}- '
                 f'{'{:<12}'.format(flight_status(flight['delay'],flight['cancelled']))}'
 )
 
@@ -44,12 +48,13 @@ def flight_status(delay,cancelled):
     
 #Part 4
 def airport_statistics():
-    cancelled, delayed, total_pass, largest_pass, filled_flights = 0,0,0,0,0
+    cancelled, delayed, total_pass, largest_pass, filled_flights, num_flights = 0,0,0,0,0,0
     for flight in flights:
         if flight['cancelled']: cancelled += 1
         if flight['delay'] > 0: delayed += 1
         if flight['passengers'] > largest_pass: largest_pass = flight['passengers']
         if flight['passengers'] / flight['max_cap'] >= 0.8: filled_flights += 1
+        if flight['passengers']: num_flights += 1
         total_pass += flight['passengers']
 
     print(  f'# of flights: {len(flights)}',
@@ -57,7 +62,7 @@ def airport_statistics():
             f'# of delayed flights: {delayed}',
             f'# of on-time flights: {len(flights)-delayed}',
             f'# of total passengers: {total_pass}',
-            f'# of average passenger per flight: {round(total_pass/len(flights))}',
+            f'# of average passenger per flight: {round(total_pass/num_flights)}',
             f'# of passengers in the highest load flight: {largest_pass}',
             f'# of flights with >80% capacity: {filled_flights}',
             sep='\n'
@@ -66,5 +71,108 @@ def airport_statistics():
 # airport_statistics()
 
 #Part 5
+def search_flight(search):
+    for flight in flights:
+        if search == flight['flight']:
+            print(  f'Destination: {flight['destination']}',
+                    f'Departure: {flight['departure']}',
+                    f'Gate: {flight['gate']}',
+                    f'Passengers: {flight['passengers']}',
+                    f'Status: {flight_status(flight['delay'],flight['cancelled'])}',
+                    sep='\n'
+            )
+            return
+    print('Flight not found.')
+
+# search_flight('KL1228')
+# search_flight('AB1234')
+
+#Part 6
+def get_gate(gate):
+    if gate:
+        return gate
+    else:
+        return 'NONE'
+
+# departure_board()
+# airport_statistics()
+
+#Part 7
+def print_gates():
+    gates = set()
+    for flight in flights:
+        if flight['gate']:
+            gates.add(flight['gate'])
+    for gate in gates:
+        print('Gate:',gate)
+
+# print_gates()
+
+#Part 8
+menu_structure = [  'AIRPORT DEPARTURE SYSTEM\n',
+                    '1. View all flights',
+                    '2. View all delayed flights',
+                    '3. View all cancelled flights',
+                    '4. Search for a flight',
+                    '5. View flight statistics',
+                    '6. Quit\n'
+]
+valid_options = ['1','2','3','4','5','6']
+
+def interactive_menu():
+    while True:
+        print('',*menu_structure,sep='\n')
+        option = input('Choose an option (1-6): ')
+        if option not in valid_options:
+            print('Please select a # 1-6 option!')
+            continue
+        elif option == '6':
+            break
+        elif option == '5':
+            airport_statistics()
+        elif option == '4':
+            search_flight(input('Enter flight to search: '))
+        elif option == '3':
+            departure_board(only_can=True)
+        elif option == '2':
+            departure_board(only_delay=True)
+        elif option == '1':
+            departure_board()
+        
+# interactive_menu()
+
+#Part 9
+#Already considered during first pass
+
+#Final Challenge
+def airport_report():
+    scheduled,cancelled,delayed,total_pass = 0,0,0,0
+    busiest, busiest_output, filled = 0,'',[]
+    for flight in flights:
+        scheduled += 1
+        if flight['cancelled']: cancelled += 1
+        if flight['delay'] > 0: delayed += 1
+        total_pass += flight['passengers']
+        if flight['passengers'] > busiest: 
+            busiest,busiest_output = flight['passengers'],f'{flight['flight']} - {flight['destination']} - {flight['passengers']}'
+        if flight['passengers'] / flight['max_cap'] >= 0.8: 
+            filled.append(f'{'{:<6}'.format(flight['flight'])} - {flight['destination']}')
+    print(  f'AIRPORT OPERATIONS REPORT\n',
+            f'Scheduled flights: {scheduled}',
+            f'Cancelled flights: {cancelled}',
+            f'Delayed flights: {delayed}',
+            f'On-Time flights: {scheduled-delayed}\n',
+            f'Passengers today: {total_pass}\n',
+            f'Busiest flight:\n{busiest_output}\n',
+            f'Flights above 80% capacity:',
+            *filled,
+            sep='\n'
+    )
+
+# airport_report()
+
+
+
+
 
 
